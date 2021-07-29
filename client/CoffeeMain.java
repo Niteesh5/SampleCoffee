7f3e7924ada887daa16442bda5a2fb128d463745
Niteesh5 <49987749+Niteesh5@users.noreply.github.com>
2021-07-29 22:30:30 +0530

    Add files via upload

---
+++ client/CoffeeMain.java
@@ -0,0 +1,12 @@
+package com.coffee.client;
+import com.coffee.presentation.*;
+public class CoffeeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CoffeePresentation coffeePresentation =new CoffeePresentation();
+		coffeePresentation.showMenu();
+		
+	}
+
+}
